462c233fcdb5248a98ffa358b8266bbb61c79e5e
Tenzin Choedak <tchoedak@gmail.com>
2019-10-28 20:23:35 -1000

    init setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='randomly-close',
+    packages=['randomly_close'],
+    version='0.0.1',
+    description='Randomly Close Key Codes',
+    author='tchoedak',
+    author_email='tchoedak@gmail.com',
+    url='https://github.com/tchoedak/randomly_close',
+)
